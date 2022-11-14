910814df1539ae8d335864a7e65f2ba4edcf8194
Benjamin Rittenhouse <benjamin.rittenhouse@temple.edu>
2022-11-14 09:03:47 -0500

    cleaned up again

--- react-spa-webapi/server/app.js
+++ react-spa-webapi/server/app.js
@@ -13,7 +13,6 @@ var path = require('path');
 const mysql = require('mysql');
 const bodyparser = require('body-parser');
 
-
 // "Cross-Origin Resource Sharing"
 //allows us to make api calls
 app.use(cors());
@@ -25,7 +24,6 @@ app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   console.log("Homepage");
-  res.send("web_user CRUD Homepage");
 });
 
 //get all user api
@@ -36,83 +34,6 @@ app.get("/api/listAllUsers", (req, res) => {
     res.send(result);
   });
 });
-
-//Router to GET specific user by ID
-app.get('/api/getUser/:id' , (req, res) => {
-  db.query('SELECT * FROM web_user WHERE web_user_id=?',[req.params.id], (req, result) => {
-    console.log("Resultt: " + result);
-    res.send(result);
-  })
-});
-
-//Router to GET specific user by ID
-app.get('/api/getUser/:id/:email' , (req, res) => {
-  db.query('SELECT * FROM web_user WHERE web_user_id=? AND user_email =?',[req.params.id, req.params.email], (req, result) => {
-    console.log("Result: " + result);
-    res.send(result);
-  })
-});
-
-// https://www.w3resource.com/node.js/nodejs-mysql.php CHECK THIS
-
-// api/insertUser/111/bbb@b.com/asdf1/img.jpg/100.0/2020-10-10/1
-app.get("/api/insertUser/:email/:password/:image/:fee/:bday/:role", (req, res) => {
-  // add insert validation ***
-  // if there are no errors (string len is 0)
-  try {
-    const sqlIns = "INSERT INTO web_user (user_email, user_password, image, membership_fee, birthday, web_user.user_role_id) values (?,?,?,?,?,?)";
-    const p = req.params;
-    db.query(sqlIns,[p.email, p.password, p.image, p.fee, p.bday, p.role], (err, req, result) => {
-      if(err){
-        res.send(err);
-        console.log("There was an error! Record not inserted!")
-      } else {
-        res.send(result);
-        console.log("No errors! Record inserted!");
-      }
-    });
-  } catch {
-    res.send("Oh no DB error");
-    console.log("console oh no db error");
-  }
-});
-
-// mini test insert
-app.get("/api/insertUser/:id/:email/:password/:role", (req, res) => {
-  try {
-    const sqlIns = "INSERT INTO web_user (web_user_id, user_email, user_password, user_role_id) values (?,?,?,?)";
-    const p = req.params;
-    db.query(sqlIns,[p.id, p.email, p.password, p.role], (err, req, result) => {
-      if(err){
-        res.send(err);
-        console.log("There was an error! Record not inserted!")
-      } else {
-        res.send(result);
-        console.log("No errors! Record inserted!");
-      }
-    });
-  } catch {
-    res.send("Oh no DB error");
-    console.log("console oh no db error");
-  }
-
-});
-
-// testing json object
-// /api/insertUser2/{'web_user_id': '111', 'user_email': 'bbb@b.com', 'user_password': 'asdf1', 'image': 'img.jpg', 'membership_fee': '100.0', 'birthday': '2020-10-10', 'user_role_id': '1'}
-
-// api/insertUser?id=555
-app.get('/api/insertUser2/id=123', (req, res) => {
-  //app.get("/api/insertUser/:id/:email/:password/:image/:fee/:bday/:role", (req, res) => {
-    console.log("hardcoded URL: " + req.query.id);
-    // line to get json data as object
-   /* const sqlIns = "INSERT INTO web_user (web_user_id, user_email, user_password, image, membership_fee, birthday, user_role_id) values (?,?,?,?,?,?,?)";
-    const p = req.params;
-    db.query(sqlIns,[p.id, p.email, p.password, p.image, p.fee, p.bday, p.role], (req, result) => {
-      console.log("Result of insert: " + result);
-      res.send(result);
-    });*/
-  });
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Listening...");
--- react-spa-webapi/server/connection/DbConn.js
+++ react-spa-webapi/server/connection/DbConn.js
@@ -1,14 +1,20 @@
 const mysql = require("mysql");
+require("dotenv").config();
 
 //Creates connection
 //create pool defaults to 10 connnections. Can change by specifying connectionLimit: #
 const db = mysql.createPool({
-    host: "us-cdbr-east-06.cleardb.net",
-    user: "b23560dbdb2fe8",
-    password: "f91b7e9b",
-    database: "heroku_73fd5bf53ffabe9",
-    connectionLimit: 50
+    host: process.env.HOST,
+    user: process.env.USER1,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
 });
+
+console.log("Host: " + process.env.HOST);
+console.log("User: " + process.env.USER1);
+console.log("Password: " + process.env.PASSWORD);
+console.log("Database: " + process.env.DATABASE);
+
 
 console.log("Database Connection");
 
