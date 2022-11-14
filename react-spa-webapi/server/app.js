89729d55cd163b595e17de2f0a1393f186bfa457
Benjamin Rittenhouse <benjamin.rittenhouse@temple.edu>
2022-11-14 09:10:30 -0500

    env variables change

--- react-spa-webapi/server/app.js
+++ react-spa-webapi/server/app.js
@@ -7,10 +7,7 @@ const app = express();
 
 const db = require("./connection/DbConn");
 
-var path = require('path');
 //***Middleware start***
-
-const mysql = require('mysql');
 const bodyparser = require('body-parser');
 
 // "Cross-Origin Resource Sharing"
@@ -28,7 +25,8 @@ app.get("/", (req, res) => {
 
 //get all user api
 app.get("/api/listAllUsers", (req, res) => {
-  const sqlGet = "SELECT * FROM web_user;";
+  const sqlGet = "SELECT web_user_id, user_email, user_password, image, membership_fee, birthday, web_user.user_role_id, role_type "
+  + " FROM web_user, user_role WHERE web_user.user_role_id = user_role.user_role_id ORDER BY web_user_id ASC";
   db.query(sqlGet, (req, result) => {
     console.log("Result: " + result);
     res.send(result);
